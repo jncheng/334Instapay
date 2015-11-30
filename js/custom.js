2b1d88ff896b544b97956da406d36513e7a33af7
jncheng <jncheng@sfu.ca>
2015-11-29 23:08:03 -0800

    uploading

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#checkIn');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('border-bottom', '1px solid #eeeeee');
+       } else {
+          $('.navbar-default').css('border', 'transparent');
+       }
+   });
+    }
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
